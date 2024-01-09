3fc2ac814197c70b7b49a86bcb2ffef1a5efae08
r0zh <r0zh017@proton.me>
2024-01-09 08:18:03 +0000

    update todo item component

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent {
-
+  item: Item = {
+    id: 1,
+    name: 'Buy Milk',
+    status: false,
+  };
 }
