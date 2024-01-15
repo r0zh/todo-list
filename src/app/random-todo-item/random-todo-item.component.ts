2478ba99da92abc34d3f5b776eb965adc8e3c296
r0zh <r0zh017@proton.me>
2024-01-15 08:05:36 +0000

    add logic to random items component

--- src/app/random-todo-item/random-todo-item.component.ts
+++ src/app/random-todo-item/random-todo-item.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-random-todo-item',
   templateUrl: './random-todo-item.component.html',
-  styleUrls: ['./random-todo-item.component.css']
+  styleUrls: ['./random-todo-item.component.css'],
 })
 export class RandomTodoItemComponent {
-
+  numTasks = 0;
+  addRandomItems() {
+    for (let i = 0; i < this.numTasks; i++) {
+      console.log('Adding random item', i + 1);
+    }
+    const randomTodoItem = {
+      title: `Random todo item ${Math.random()}`,
+      completed: false,
+    };
+  }
 }
