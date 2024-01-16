4a66b44547c3a19b2ded2db76a505aa2814a4f0a
r0zh <r0zh017@proton.me>
2024-01-16 10:09:21 +0000

    add list animation to todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodoModule } from './todo/todo.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent, TodoModule],
 })
--- src/app/todo/todo-item-list/todo-item-list.component.ts
+++ src/app/todo/todo-item-list/todo-item-list.component.ts
@@ -2,10 +2,35 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Item } from '../../interfaces/item';
 import { ITEMS } from '../mock-items';
 import { LocalStorageService } from '../../services/localStorageService';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
+
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(
+      ':enter',
+      [
+        style({ opacity: 0 }),
+        stagger('60ms', animate('600ms ease-out', style({ opacity: 1 }))),
+      ],
+      { optional: true }
+    ),
+    query(':leave', animate('200ms', style({ opacity: 0 })), {
+      optional: true,
+    }),
+  ]),
+]);
 
 @Component({
   selector: 'app-todo-item-list',
   templateUrl: './todo-item-list.component.html',
+  animations: [listAnimation],
 })
 export class TodoItemListComponent {
   constructor(private localStorageService: LocalStorageService) {}
