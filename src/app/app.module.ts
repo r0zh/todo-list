f8aa5c4be667f5240345efb688aac64ec2c753c3
r0zh <r0zh017@proton.me>
2024-01-30 13:13:37 +0100

    refactor(app-module): remove unused TodoComponent import from AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/pages/todo.component';
 import { TodoModule } from './todo/todo.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
