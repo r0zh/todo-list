2d55ed89584a0a1f4a09c552a784dafe17fcb146
r0zh <r0zh017@proton.me>
2024-02-20 20:43:59 +0100

    refactor: remove unused user endpoint

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ItemController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +14,5 @@ use App\Http\Controllers\Api\ItemController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('items', ItemController::class);
+
