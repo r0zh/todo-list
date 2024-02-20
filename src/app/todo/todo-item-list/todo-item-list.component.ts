76fda5bf7d2ba91b544a3eb8a2d31b42b85d565a
r0zh <r0zh017@proton.me>
2024-02-20 20:08:42 +0100

    fix: remove animations

--- src/app/todo/todo-item-list/todo-item-list.component.ts
+++ src/app/todo/todo-item-list/todo-item-list.component.ts
@@ -1,36 +1,10 @@
 import { Component } from '@angular/core';
 import { Item } from '../../interfaces/item';
 import { TodoService } from '../../services/todo.service';
-import {
-  trigger,
-  transition,
-  style,
-  animate,
-  query,
-  stagger,
-} from '@angular/animations';
-
-// Animation for list
-const listAnimation = trigger('listAnimation', [
-  transition('* <=> *', [
-    query(
-      ':enter',
-      [
-        style({ opacity: 0 }),
-        stagger('60ms', animate('600ms ease-out', style({ opacity: 1 }))),
-      ],
-      { optional: true }
-    ),
-    query(':leave', animate('200ms', style({ opacity: 0 })), {
-      optional: true,
-    }),
-  ]),
-]);
 
 @Component({
   selector: 'app-todo-item-list',
   templateUrl: './todo-item-list.component.html',
-  animations: [listAnimation],
 })
 export class TodoItemListComponent {
   constructor(private TodoService: TodoService) { }
