358967028cdfd3932ffb94fa458a673836927610
r0zh <r0zh017@proton.me>
2024-02-20 13:53:00 +0100

    feat(api): add item resource controller and validation request, register new routes

---
+++ laravel-api/app/Http/Controllers/Api/ItemController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json([
+        'status' => true,
+        'items' => $items
+        ]);
+    }
+
+    public function store(StoreItemRequest $request)
+    {
+        $item = Item::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => "Item Created successfully!",
+            'item' => $item
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Item $item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Item $item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Item $item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Item $item)
+    {
+        //
+    }
+}
