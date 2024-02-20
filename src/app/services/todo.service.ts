06adcb0b6176b611c0bce1a023d61c8292e4e4d1
r0zh <r0zh017@proton.me>
2024-02-20 18:53:18 +0100

    refactor(todo.service): replace IndexedDbService with ApiService and update methods

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { IndexedDbService } from './indexed-db.service';
+import { ApiService } from './laravel.service';
 import { Item } from '../interfaces/item';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,70 +15,76 @@ export class TodoService {
   items: Item[] = [];
   private _items: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>([]);
   items$ = this._items.asObservable();
-  private _item: Subject<Item> = new Subject<Item>();
-  item$ = this._item.asObservable();
 
-  constructor(private indexedDbService: IndexedDbService) {
-    this.loadInitialData();
+  constructor(private apiService: ApiService) {
+    this.loadItems();
   }
 
-  async loadInitialData() {
-    const items = await this.indexedDbService.getAllItems();
-    this._items.next(items);
+  loadItems() {
+    this.apiService.getAllItems().pipe(
+      tap((items: Item[]) => {
+        console.log(items)
+        this._items.next(items);
+      })
+    ).subscribe();
+    console.log("La Consulta a la BD devuelve: ", this._items);
   }
 
   /**
   * Adds a new item to the list of items.
   * @param item - The item to be added.
   */
-  async addItem(item: Item) {
+  addItem(item: Item) {
     console.log('addItem', item);
-    const currentItems = this._items.getValue();
-    await this.indexedDbService.addItem(item);
-    // get id of the last item added
-    const items = await this.indexedDbService.getAllItems();
-    item.id = items[items.length - 1].id;
-    this._items.next([...currentItems, item]);
+    this.apiService.addItem(item).subscribe({
+      next: () => {
+        this.loadItems(); // Recargar la lista completa desde el servidor
+      },
+      error: (error: any) => {
+        console.error("Error al añadir personaje:", error);
+      }
+    });
   }
 
   /**
     * Removes an item from the list of items.
     * @param item - The item to be removed.
     */
-  async removeItem(item: Item) {
+  removeItem(item: Item) {
     const id = item.id!;
     console.log('removeItem', id);
-    const currentItems = this._items.getValue();
-    const index = currentItems.findIndex(itemInArray => itemInArray.id == item.id);
-    currentItems.splice(index, 1);
-    await this.indexedDbService.deleteItem(id); // Elimina de IndexedDB
+    this.apiService.removeItem(id).subscribe({
+      next: () => {
+        this.loadItems(); // Recargar la lista completa desde el servidor
+      },
+      error: (error: any) => {
+        console.error("Error al borrar personaje:", error);
+      }
+    });
   }
 
   /**
     * Changes the status of an item.
     * @param item - The item whose status is to be changed.
     */
-  async changeStatus(item: Item) {
+  changeStatus(item: Item) {
     console.log('changeStatus', item);
-    const currentItems = this._items.getValue();
-    const index = currentItems.findIndex(itemInArray => itemInArray.id == item.id);
-    currentItems[index].status = item.status;
-    await this.indexedDbService.updateItems(currentItems);
-    await this.loadInitialData(); // Actualiza la lista de items
+    if (item.id) {
+      this.apiService.updateItem(item);
+      this.loadItems(); // Actualiza la lista de items
+    }
   }
 
   /**
     * Changes the name of an item.
     * @param item - The item whose name is to be changed.
     */
-  async changeName(item: Item) {
+  changeName(item: Item) {
     console.log('changeName', item);
-    const currentItems = this._items.getValue();
-    const index = currentItems.findIndex(itemInArray => itemInArray.id == item.id);
-    currentItems[index].name = item.name;
-
-    await this.indexedDbService.updateItem(item.id!, item);
-    await this.loadInitialData(); // Actualiza la lista de items
+    if (item.id) {
+      this.apiService.updateItem(item);
+      this.loadItems(); // Actualiza la lista de items
+    }
   }
 
   /**
