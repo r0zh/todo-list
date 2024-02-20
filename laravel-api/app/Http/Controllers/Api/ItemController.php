5ada7a9a941f39a5a023fccd0b856a03d04e2f2d
r0zh <r0zh017@proton.me>
2024-02-20 16:14:49 +0100

    feat(docker): add docker-compose.yml and remove duped store method

--- laravel-api/app/Http/Controllers/Api/ItemController.php
+++ laravel-api/app/Http/Controllers/Api/ItemController.php
@@ -40,14 +40,6 @@ class ItemController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Item $item)
