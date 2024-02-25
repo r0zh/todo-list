d5a05d9175896fde0c3c557a7abf4cc57d85f180
r0zh <r0zh017@proton.me>
2024-02-25 16:49:46 +0100

    feat: add position attribute for random item generator

--- src/app/todo/random-todo-item/random-todo-item.component.ts
+++ src/app/todo/random-todo-item/random-todo-item.component.ts
@@ -19,7 +19,8 @@ export class RandomTodoItemComponent {
     for (let i = 0; i < this.numTasks; i++) {
       let randomItem =
         RANDOM_ITEMS[Math.floor(Math.random() * RANDOM_ITEMS.length)];
-      await this.TodoService.addItem({ ...randomItem });
+      randomItem.position = this.TodoService.getLastPosition();
+      this.TodoService.addItem({ ...randomItem });
     }
   }
 }
